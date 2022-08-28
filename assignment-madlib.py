8cb71a4e7b937f2ea02ce514977b2a005e444453
Brian <112209983+Babaganouche622@users.noreply.github.com>
2022-08-27 21:57:36 -0700

    Add files via upload

---
+++ assignment-madlib.py
@@ -0,0 +1,67 @@
+# This is the introduction speech in Elden Ring:
+
+
+# The fallen leaves tell a story.
+# The great Elden Ring was shattered.
+# In our home, across the fog, the Lands Between.
+# Now, Queen Marika the Eternal is nowhere to be found,
+# and in the Night of the Black Knives, Godwyn the Golden was the first to perish.
+# Soon, Marika's offspring, demigods all, claimed the shards of the Elden Ring.
+# The mad taint of their newfound strength triggered the Shattering.
+# A war from which no lord arose.
+# A war leading to abandonment by the Greater Will.
+# Arise now, ye Tarnished.
+# Ye dead, who yet live.
+# The call of long-lost grace speaks to us all.
+# Hoarah Loux, chieftan of the badlands.
+# The ever-brilliant Goldmask.
+# Fia, the Deathbed Companion.
+# The loathsome Dung Eater.
+# And Sir Gideon Ofnir, the All-knowing.
+# And one other. Whom grace would again bless.
+# A Tarnished of no renown.
+# Cross the fog, to the Lands Between.
+# To stand before the Elden Ring.
+# And become the Elden Lord.
+
+print("Let's play a game.")
+
+input_elden = input("Enter noun")
+input_lands = input("Enter noun, place")
+input_queen = input("Enter adjective")
+input_marika = input("Enter name")
+input_godwyn = input("Enter name")
+input_perish = input("Enter verb")
+input_greater_will = input("Enter name")
+input_tarnished = input("Enter noun")
+input_hoarah = input("Enter name")
+input_chieftan = input("Enter adjective")
+input_goldmask = input("Enter name")
+input_fia = input("Enter name")
+input_loathsome = input("enter adjective")
+input_knowing = input("Enter adjective")
+
+print("The fallen leaves tell a story.")
+print("The great " + input_elden + " Ring was shattered.")
+print("In our home, across the fog, the " + input_lands + " Between.")
+print("Now, " + input_queen + " " + input_marika + " the Eternal is nowhere to be found,")
+print("and in the Night of the Black Knives, " + input_godwyn + " the Golden was the first to perish.")
+print("Soon, " + input_marika + "'s offspring, demigods all, claimed the shards of the " + input_elden + " Ring.")
+print("The mad taint of their newfound strength triggered the Shattering.")
+print("A war from which no lord arose.")
+print("A war leading to abandonment by the Greater Will.")
+print("Arise now, ye " + input_tarnished + ".")
+print("Ye dead, who yet live.")
+print("The call of long-lost grace speaks to us all.")
+print(input_hoarah + ", " + input_chieftan + " of the badlands.")
+print("The ever-brilliant " + input_goldmask + ".")
+print(input_fia + ", the Deathbed Companion.")
+print("The " + input_loathsome + " Dung Eater.")
+print("And Sir Gideon Ofnir, the " + input_knowing + ".")
+print("And one other. Whom grace would again bless.")
+print("A " + input_tarnished + " of no renown.")
+print("Cross the fog, to the " + input_lands + " Between.")
+print("To stand before the " + input_elden + " Ring.")
+print("And become the " + input_elden + " Lord.")
+
+print("Thanks for playing!")
